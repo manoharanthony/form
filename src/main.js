da643a0bd0e2fbcd4df2cbb511409da3992e2ea6
manoharanthony <manohar.anthony@wohlig.in>
2019-09-13 14:49:48 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import BootstrapVue from "bootstrap-vue";
+import Vuelidate from "vuelidate";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+Vue.use(Vuelidate);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <formSection/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import formSection from "@/components/form.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+    formSection
+  }
+};
+</script>
